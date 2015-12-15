6c7712b472c86c87ac125ab986b9c5f646646733
Yury <andreevym@gmail.com>
2015-12-15 17:08:28 +0300

    PropertySourcesPlaceholderConfigurer - add for annotation @Value
    AuthenticationTests - testing authentication
    
    ldap.properties - setting for ldap test server
    
    for MockMvc test
    I add dependency groupId:org.springframework.security, artifactId: spring-security-test

--- src/main/java/ru/joinlang/config/LdapConfiguration.java
+++ src/main/java/ru/joinlang/config/LdapConfiguration.java
@@ -1,8 +1,10 @@
 package ru.joinlang.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.ldap.core.support.LdapContextSource;
@@ -13,13 +15,33 @@ public class LdapConfiguration {
     @Autowired
     Environment env;
 
+    @Value("${ldap.url}")
+    String ldapUrl;
+
+    @Value("${ldap.base}")
+    String ldapBase;
+
+    @Value("${ldap.user}")
+    String ldapUser;
+
+    @Value("${ldap.password}")
+    String ldapPassword;
+
+    /**
+     * for @Value with properties
+     */
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfigIn() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
     @Bean
     public LdapContextSource ldapContextSource() {
         LdapContextSource ldapContextSource = new LdapContextSource();
-        ldapContextSource.setUrl(env.getProperty("ldap.url"));
-        ldapContextSource.setBase(env.getProperty("ldap.base"));
-        ldapContextSource.setUserDn(env.getProperty("ldap.user"));
-        ldapContextSource.setPassword(env.getProperty("ldap.password"));
+        ldapContextSource.setUrl(ldapUrl);
+        ldapContextSource.setBase(ldapBase);
+        ldapContextSource.setUserDn(ldapUser);
+        ldapContextSource.setPassword(ldapPassword);
         ldapContextSource.afterPropertiesSet();
         return ldapContextSource;
     }
